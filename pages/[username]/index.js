7ec11181d2703f7b80f7242f3929e63a22fdeb69
Daniel <mr_schliesing@hotmail.com>
2022-03-26 21:35:54 -0800

    start of project

---
+++ pages/[username]/index.js
@@ -0,0 +1,7 @@
+export default function UserProfilePage({}) {
+    return(
+        <main>
+            <h1>user prof page</h1>
+        </main>
+    )
+}
---
+++ pages/admin/[slug].js
@@ -0,0 +1,9 @@
+//dynamic url page
+
+export default function AdminPostEdit({}) {
+    return(
+        <main>
+            <h1>Edit Post</h1>
+        </main>
+    )
+}
